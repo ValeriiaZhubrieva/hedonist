9c3d4a5e1d3c2a611e55b298f64bd68e10953e07
ValeriiaZhubrieva <67803539+ValeriiaZhubrieva@users.noreply.github.com>
2024-09-28 23:32:48 +0300

    add mailer

---
+++ files/sendmail/sendmail.php
@@ -0,0 +1,71 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+	require 'phpmailer/src/SMTP.php';
+
+	$name = trim($_POST['name']);
+	$tel = trim($_POST['tel']);
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('uk', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	/*
+	$mail->isSMTP();                                            //Send using SMTP
+	$mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+	$mail->Username   = 'user@example.com';                     //SMTP username
+	$mail->Password   = 'secret';                               //SMTP password
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+	$mail->Port       = 465;                 
+	*/
+
+	//Від кого лист
+	$mail->setFrom('Lviv.krym@gmail.com'); // Вказати потрібний E-mail
+	//Кому відправити
+	$mail->addAddress('starbud.marketing@gmail.com'); // Вказати потрібний E-mail
+	//Тема листа
+	$mail->Subject = 'Запис на огляд';
+
+	//Тіло листа
+	$body = '<h1>Запис на огляд</h1>';
+
+	if(trim(!empty($_POST['name']))){
+		$body .='<p><strong>Name:</strong>'.$name.'</p>';
+	}
+	if(trim(!empty($_POST['tel']))){
+		$body .='<p><strong>Phone:</strong>'.$tel.'</p>';
+	}
+	
+	/*
+	//Прикріпити файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//шлях завантаження файлу
+		$filePath = __DIR__ . "/files/sendmail/attachments/" . $_FILES['image']['name']; 
+		//грузимо файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Фото у додатку</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
+	*/
+
+	$mail->Body = $body;
+
+	//Відправляємо
+	if (!$mail->send()) {
+		$message = 'Помилка';
+	} else {
+		$message = 'Дані надіслані!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
